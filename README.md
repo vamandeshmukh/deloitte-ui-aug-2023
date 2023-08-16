e4e236f2bf4c6e9d9f9ace0c109fa12e7fae8e23
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-16 12:52:40 +0530

    js

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 # Link to the documentation: 
 [HTML CSS PPTs](https://docs.google.com/document/d/1RAI44-NkiJYRexHQAeGLIo-lCzF5KnRddHq9rW0N0JI/edit?usp=sharing) 
+
+
+[JavaScript PPTs](https://docs.google.com/document/d/1qOndyRUzdS59OIQHmX2NAFf5QVZAHeQjF1kk0-scUrY/edit?usp=sharing) 
+
+
--- js/01-js-basics.js
+++ js/01-js-basics.js
@@ -14,20 +14,25 @@
 // let num2 = 20;
 // num2 = "abc";
 
-
 let someVar = 10;
 console.log(someVar);
-
+someVar = 20;
+console.log(someVar);
+console.log(typeof (someVar));
 someVar = "abc";
-
 console.log(someVar);
-
+console.log(typeof (someVar));
 someVar = false;
-
 console.log(someVar);
+console.log(typeof (someVar));
 
 
+// Datatypes in JS - 
+// primitive datatypes 
+// reference datatypes 
 
+// primitive datatypes - number, string, boolean 
+// reference datatypes - array, object, class, function, ... 
 
 
 
