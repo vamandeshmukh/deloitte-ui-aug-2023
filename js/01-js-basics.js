99bafcc5a0690692b99590d6926fe9dd607ede97
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-16 12:37:03 +0530

    js

--- js/01-js-basics.js
+++ js/01-js-basics.js
@@ -1,4 +1,34 @@
 // JS basics 
 
-console.log(10 + 2);
+// console.log(10 + 2);
+
+// 1. variable declaration - 
+
+// java / c - strongly typed languages 
+// int num = 10;
+// num = "abc";
+// String name = "Sonu";
+
+// JS - dynamically typed language 
+// const num1 = 10;
+// let num2 = 20;
+// num2 = "abc";
+
+
+let someVar = 10;
+console.log(someVar);
+
+someVar = "abc";
+
+console.log(someVar);
+
+someVar = false;
+
+console.log(someVar);
+
+
+
+
+
+
 
