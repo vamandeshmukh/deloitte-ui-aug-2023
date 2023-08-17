c9695c67829c8d056bf4698dcb7eef8d9571b360
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-17 10:41:04 +0530

    js

--- js/04-js-functions.js
+++ js/04-js-functions.js
@@ -16,24 +16,40 @@
 // fun2();
 
 
-// function syntax 
+// // function syntax 
 
-// const addNums2 = 'abc';
-// const addNums3 = 10;
-// const addNums4 = false;
-
-// // full syntax 
-// const addGst = (amount) => {
-//     return amount + (amount * 0.18);
-// };
+// // // full syntax 
+// // const addGst = (amount) => {
+// //     return amount + (amount * 0.18);
+// // };
 
 // // // short syntax 
-// const addGst = (amount) => amount + (amount * 0.18);
+// // const addGst = (amount) => amount + (amount * 0.18);
 
-// shortest syntax 
-const addGst = amount => amount + (amount * 0.18);
+// // shortest syntax 
+// const addGst = amount => amount + (amount * 0.18);
 
-let finalAmount = addGst(100);
-console.log(finalAmount);
+// let finalAmount = addGst(100);
+// console.log(finalAmount);
+
+
+
+// // function arguments 
+
+// const addNums = (i, j) => { console.log(i + j); };
+
+// addNums();
+// addNums(10);
+// addNums(10, 20);
+// addNums(10, 20, 30);
+
+// default values to parameters 
+
+const addNums = (i = 2, j = 4) => { console.log(i + j); };
+
+addNums();
+addNums(10);
+addNums(10, 20);
+addNums(10, 20, 30);
 
 
