ab51fa893c93b8666230d8c5cafd4c80b4cdfc30
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-17 10:14:57 +0530

    js

---
+++ js/04-js-functions.js
@@ -0,0 +1,18 @@
+
+// JS functions 
+
+// Old JS function 
+function fun() {
+    console.log('fun function called.');
+};
+
+fun();
+
+// Modern JS function / arrow function / fat arrow function 
+const fun2 = () => {
+    console.log('fun2 function called.');
+};
+
+fun2();
+
+
