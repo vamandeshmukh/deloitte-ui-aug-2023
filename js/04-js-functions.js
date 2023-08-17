50120a7b926576e1ae0a2d3500e8152e2865d01c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-17 10:24:58 +0530

    js

--- js/04-js-functions.js
+++ js/04-js-functions.js
@@ -1,18 +1,39 @@
 
 // JS functions 
 
-// Old JS function 
-function fun() {
-    console.log('fun function called.');
-};
+// // Old JS function 
+// function fun() {
+//     console.log('fun function called.');
+// };
 
-fun();
+// fun();
 
-// Modern JS function / arrow function / fat arrow function 
-const fun2 = () => {
-    console.log('fun2 function called.');
-};
+// // Modern JS function / arrow function / fat arrow function 
+// const fun2 = () => {
+//     console.log('fun2 function called.');
+// };
 
-fun2();
+// fun2();
+
+
+// function syntax 
+
+// const addNums2 = 'abc';
+// const addNums3 = 10;
+// const addNums4 = false;
+
+// // full syntax 
+// const addGst = (amount) => {
+//     return amount + (amount * 0.18);
+// };
+
+// // // short syntax 
+// const addGst = (amount) => amount + (amount * 0.18);
+
+// shortest syntax 
+const addGst = amount => amount + (amount * 0.18);
+
+let finalAmount = addGst(100);
+console.log(finalAmount);
 
 
