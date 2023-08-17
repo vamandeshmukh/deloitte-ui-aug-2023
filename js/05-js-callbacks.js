8e35090f3289b5a6a7f9e46cd8349306b368f99d
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-17 12:26:06 +0530

    js

--- js/05-js-callbacks.js
+++ js/05-js-callbacks.js
@@ -1,7 +1,7 @@
 
 
 // JS - asynchronous nature of JS 
-// asynchronous == not synchronous
+// asynchronous == not synchronous // non-blocking 
 
 // console.log('one');
 // console.log('two'); // delayed execution - 2 sec 
@@ -12,14 +12,39 @@
 
 // setTimeout(() => {}, delay-time);
 
-console.log('one');
+// console.log('one');
 
-setTimeout(() => {
-    console.log('two');
-}, 2000);
+// setTimeout(() => {
+//     console.log('two');
+// }, 2000);
 
-console.log('three');
+// console.log('three');
 
+
+// problem due to asynchronous JS 
+
+// const fun = () => {
+//     setTimeout(() => {
+//         return { message: 'Have fun!' };
+//     }, 2000);
+// };
+
+// let output = fun();
+// console.log(output.message);
+
+// solution 1 - callback 
+
+const fun = (arg) => {
+    setTimeout(() => {
+        arg({ message: 'Have fun!' });
+    }, 2000);
+};
+
+// fun((a) => { });
+
+fun((a) => {
+    console.log(a.message);
+});
 
 
 
