f7ab5050f6e59b168afc6d2ab0a9ffec6b7c7462
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-18 10:46:56 +0530

    js

--- assgn/script.js
+++ assgn/script.js
@@ -1,6 +1,14 @@
 const welcomeUser = () => {
-    const username = document.getElementById('username').value;
+    const username = document.getElementById('abc').value;
     console.log(username);
-    document.getElementById('welcome-message').innerHTML = `Welcome ${username}!`;
+    document.getElementById('welcome-message').innerHTML = `<span>some text</span>`;
+    document.getElementById('welcome-message').textContent = `Welcome ${username}!`;
     document.getElementById('welcome-form').reset();
+    // window.alert(`Hi ${username}!`);
 };
+
+// const welcomeUser = () => {
+//     document.getElementById('welcome-message').innerHTML =
+//         `Welcome ${document.getElementById('abc').value}!`;
+//     document.getElementById('welcome-form').reset();
+// };
--- js/script.js
+++ js/script.js
@@ -6,35 +6,39 @@ const empList = [
     { eid: 104, firstName: 'Ponu', salary: 99000 }
 ];
 
-// empList.forEach(e => console.log(e.eid, e.firstName, e.salary));
+empList.forEach(e => console.log(e.eid, e.firstName, e.salary));
 
 // display this data in the HTML table 
 
 const empData = document.getElementById('emp-data');
 
 // // solution 1 - using string concat looping  
-// let temp = '';
-// empList.forEach((emp) => {
-//     console.log(emp);
-//     temp += `<tr> 
-//     <td>${emp.eid}</td>
-//     <td>${emp.firstName}</td>
-//     <td>${emp.salary}</td>
-//     </tr>`;
-// });
-// empData.innerHTML = temp;
+let temp = '';
 
-// solution 2 - using createElement and appendChild methods 
 empList.forEach((emp) => {
     console.log(emp);
-    const row = document.createElement('tr');
-    row.innerHTML = `<td>${emp.eid}</td><td>${emp.firstName}</td><td>${emp.salary}</td>`;
-    empData.appendChild(row);
+    temp += `<tr> 
+    <td>${emp.eid}</td>
+    <td>${emp.firstName}</td>
+    <td>${emp.salary}</td>
+    </tr>`;
 });
 
+console.log(temp);
+empData.innerHTML = temp;
+// empData.textContent = temp;
+
+// solution 2 - using createElement and appendChild methods
+// empList.forEach((emp) => {
+//     console.log(emp);
+//     const row = document.createElement('tr');
+//     row.innerHTML = `<td>${emp.eid}</td><td>${emp.firstName}</td><td>${emp.salary}</td>`;
+//     empData.appendChild(row);
+// });
 
 
-// another example 
+
+// another example
 // const blogData = document.getElementById('blog-data');
 // fetch('https://jsonplaceholder.typicode.com/posts')
 //     .then(resp => resp.json())
