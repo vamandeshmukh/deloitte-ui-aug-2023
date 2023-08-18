300d18283d77c6e111e47ffca8fcd1a6f6a981bb
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-08-18 11:53:58 +0530

    node

--- nodejs/nodejs-demo/index.js
+++ nodejs/nodejs-demo/index.js
@@ -8,18 +8,31 @@
 // to stop, on CMD - 
 // Ctrl + C  
 
-
 const express = require('express');
 const app = express();
 const port = 9090;
 
+// app.get('/url', (request, response) => {});
+
 app.get('/', (req, res) => {
     res.send('Welcome to My App');
     console.log('welcome');
-
 });
+
+app.get('/contact', (req, res) => {
+    res.send('Contact us for more info');
+    console.log('contact');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About our company...');
+    console.log('about');
+});
+
+// app.listen(port Number, () => {});
 
 app.listen(port, () => {
     console.log(`App started on http://localhost:${port}/.`);
 });
+
 
